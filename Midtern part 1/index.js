4aebfe24db64220d63ed79ea6c2013a70b29df20
Cdodu <43279859+Cdodu@users.noreply.github.com>
2018-11-16 15:47:29 -0500

    Add files via upload

---
+++ Midtern part 1/index.js
@@ -0,0 +1,48 @@
+const { Button, ImageView, ui } = require('tabris')
+
+const widthPhoto = 300
+const heightPhoto = 400
+
+function takePhoto () {
+  let onSuccess = image => imgPhoto.image = image
+  let onFail = message => console.log('Camera failed because: ' + message)
+  navigator.camera.getPicture(onSuccess, onFail, {
+    quality: 50,
+    targetWidth: widthPhoto,
+    targetHeight: heightPhoto,
+    destinationType: window.Camera.DestinationType.FILE_URI
+  })
+}
+
+function takeMonkey () {
+  let onSuccess = image => imgPhoto.image = 'https://raw.githubusercontent.com/mrmccormack/imd-learning-tabris/master/images/monkey.jpg'
+  let onFail = message => console.log('Camera failed because: ' + message)
+  navigator.camera.getPicture(onSuccess, onFail, {
+    quality: 50,
+    targetWidth: widthPhoto,
+    targetHeight: heightPhoto,
+    destinationType: window.Camera.DestinationType.FILE_URI
+  })
+}
+
+let btnShutter = new Button({ centerX: 0, top: 40, text: 'Take Picture' })
+  .on('select', () => {
+    takePhoto()
+  
+  })
+
+  .on('longpress', () => {
+    takeMonkey()
+  
+  }).appendTo(ui.contentView)
+
+let imgPhoto = new ImageView({
+  top: 'prev() 10',
+  class: 'image',
+  width: widthPhoto,
+  height: heightPhoto,
+  centerX: 0,
+  image: 'https://raw.githubusercontent.com/mrmccormack/imd-learning-tabris/master/images/photo.png',
+  scaleMode: 'fit',
+  zoomEnabled: true
+}).appendTo(ui.contentView)
